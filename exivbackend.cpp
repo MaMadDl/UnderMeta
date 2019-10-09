86bbae84d543891628134c2f68a061818ea40fde
Mohammad Ali Gholami <mamaddl@live.com>
2019-10-09 18:28:02 +0330

    Added Function to use Exiv Library For IMAGE files

---
+++ exivbackend.cpp
@@ -0,0 +1,94 @@
+#include "exivbackend.h"
+
+exivBackend::exivBackend(QObject *parent) : QObject(parent),_signalOutput(new QWidget)
+{
+
+}
+
+void exivBackend::newImage(QString path)
+{
+    _scroller= new QScrollArea();
+    _signalOutput=new QWidget(_scroller);
+    QGridLayout *frame=new QGridLayout(_signalOutput);
+
+    try{
+    ex2::Image::UniquePtr image=ex2::ImageFactory::open(path.toStdString());
+    if(image.get() == nullptr)
+        return;
+    image->readMetadata();
+    _exifD = image->exifData();
+    _xmpD  = image->xmpData();
+    _iptcD = image->iptcData();
+    if (!_exifD.empty()) {
+        int count=0;
+        std::cout<<"Exif Scope\n";
+        ex2::ExifData::const_iterator end=_exifD.end();
+        for (ex2::ExifData::const_iterator i= _exifD.begin(); i != end; ++i) {
+            if(i->tagLabel()!=""){
+                /*
+                std::cout<<i->tagLabel()<<"\t"
+                     << "0x" << std::setw(4) << std::setfill('0') << std::right
+                     << std::hex << i->tag() << std::dec << "\t"
+                     << i->groupName() << "\t"
+                     << i->typeName() << "\t"
+                     << i->count() << "\t"
+                     << i->print(&_exifD)
+                     << "\n";
+                     */
+
+                QLabel *lbl=new QLabel(QString(i->tagLabel().c_str()));
+                frame->addWidget(lbl,count,0,Qt::AlignLeft);
+            }
+            else {
+                QLabel *lbl=new QLabel(QString("0x")+QString::number(i->tag(),16));
+                frame->addWidget(lbl,count,0,Qt::AlignLeft);
+
+            }
+            QLabel *ltag=new QLabel(QString::fromStdString(i->print()).trimmed());
+            if(ltag->text().size()>40){
+//                ltag->text().chop(ltag->text().size()-40);
+                ltag->setText(ltag->text().remove(40,ltag->text().size()-40));
+                ltag->setWordWrap(true);
+            }
+            frame->addWidget(ltag,count,1,Qt::AlignLeft);
+
+            ///TODO add stuff for other types
+            QPushButton *btn=new QPushButton("...");
+            frame->addWidget(btn,count,2,Qt::AlignRight);
+            count++;
+        }
+
+        _scroller->setWidget(_signalOutput);
+        emit MetaTypeDone(_scroller,1);
+    }
+
+    ///TODO ADD for XMP and IPTC
+    ///    _scroller= new QScrollArea();
+    ///    _signalOutput=new QWidget(_scroller);
+    ///    QGridLayout *frame=new QGridLayout(_signalOutput);
+
+    if(!_xmpD.empty()){
+        std::cout<<"XMP Scope\n";
+        ex2::XmpData::const_iterator end=_xmpD.end();
+        for (ex2::XmpData::const_iterator i= _xmpD.begin(); i != end; ++i) {
+            std::cout << i->key() << "\t"
+                                  << i->typeName() << "\t"
+                                  << i->count() << "\t"
+                                  << "\n";
+        }
+    }
+
+    if(!_iptcD.empty()){
+        std::cout<<"IPTC Scope\n";
+        ex2::IptcData::const_iterator end=_iptcD.end();
+        for (ex2::IptcData::const_iterator i= _iptcD.begin(); i != end; ++i) {
+            std::cout << i->key() << "\n";
+        }
+    }
+    }
+    catch(ex2::AnyError& e)
+    {
+        std::cout << "Caught Exiv2 exception '" << e << "'\n";
+    }
+    return;
+}
---
+++ exivbackend.h
@@ -0,0 +1,40 @@
+#ifndef EXIVBACKEND_H
+#define EXIVBACKEND_H
+
+#include <QObject>
+#include <iostream>
+#include <exiv2/exif.hpp>
+#include <exiv2/image.hpp>
+#include <exiv2/error.hpp>
+
+#include <QGridLayout>
+#include <QPushButton>
+#include <QLabel>
+#include <QScrollArea>
+
+namespace ex2 = Exiv2;
+
+class exivBackend : public QObject
+{
+    Q_OBJECT
+public:
+    explicit exivBackend(QObject *parent = nullptr);
+
+signals:
+
+    void MetaTypeDone(QScrollArea* output,int index);
+public slots:
+
+    void newImage(QString path);
+
+private:
+    ex2::ExifData _exifD;
+    ex2::XmpData _xmpD;
+    ex2::IptcData _iptcD;
+
+    QWidget *_signalOutput;
+    QScrollArea *_scroller;
+
+};
+
+#endif // EXIVBACKEND_H
