906ce27ee42a4254dc685eb2dfe4c8d48dc24bc6
Mohammad Ali Gholami <mamaddl@live.com>
2019-11-20 14:22:46 +0330

    Added XMP Parsing Tab

--- exivbackend.cpp
+++ exivbackend.cpp
@@ -10,7 +10,6 @@ void exivBackend::newImage(QString path)
     _scroller= new QScrollArea();
     _signalOutput=new QWidget(_scroller);
     QGridLayout *frame=new QGridLayout(_signalOutput);
-
     try{
     ex2::Image::UniquePtr image=ex2::ImageFactory::open(path.toStdString());
     if(image.get() == nullptr)
@@ -63,27 +62,48 @@ void exivBackend::newImage(QString path)
             frame->addWidget(btn,count,2,Qt::AlignRight);
             count++;
         }
-
         _scroller->setWidget(_signalOutput);
         emit MetaTypeDone(_scroller,1);
     }
-
-    ///TODO ADD for XMP and IPTC
-    ///    _scroller= new QScrollArea();
-    ///    _signalOutput=new QWidget(_scroller);
-    ///    QGridLayout *frame=new QGridLayout(_signalOutput);
-    ///
-    ///
-
+    //reInitilize for next use
+    _scroller= new QScrollArea();
+    _signalOutput=new QWidget(_scroller);
+    frame=new QGridLayout(_signalOutput);
     if(!_xmpD.empty()){
         std::cout<<"XMP Scope\n";
+        int count=0;
         ex2::XmpData::const_iterator end=_xmpD.end();
         for (ex2::XmpData::const_iterator i= _xmpD.begin(); i != end; ++i) {
-            std::cout << i->key() << "\t"
-                                  << i->typeName() << "\t"
-                                  << i->count() << "\t"
-                                  << "\n";
+            //std::cout << i->key() << "\t" << i->typeName() << "\t" << i->count() << "\t" << "\n";
+            if(i->tagLabel()!=""){
+                QLabel *lbl=new QLabel(QString(i->tagLabel().c_str()));
+                frame->addWidget(lbl,count,0,Qt::AlignLeft);
+            }
+            else{
+                QLabel *lbl=new QLabel(QString("0x")+QString::number(i->tag(),16));
+                frame->addWidget(lbl,count,0,Qt::AlignLeft);
+            }
+
+            QLabel *ltag=new QLabel(QString::fromStdString(i->print()).trimmed());
+            if(ltag->text().size()>40){
+                ltag->setText(ltag->text().remove(40,ltag->text().size()-40));
+                ltag->setWordWrap(true);
+            }
+            frame->addWidget(ltag,count,1,Qt::AlignLeft);
+
+            QtMaterialFlatButton *btn=new QtMaterialFlatButton("...");
+            btn->setFontSize(16);
+            btn->setHaloVisible(false);
+            btn->setForegroundColor(QColor(255,255,255));
+
+            ///TODO add Button Signals connections
+            frame->addWidget(btn,count,2,Qt::AlignRight);
+            count++;
+
         }
+        _scroller->setWidget(_signalOutput);
+        emit MetaTypeDone(_scroller,2);
+    }
     }
 
     if(!_iptcD.empty()){
